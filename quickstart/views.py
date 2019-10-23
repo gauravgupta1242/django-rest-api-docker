df2a40b37f100752b54bfb3b849cd2b48304349b
GAURAV GUPTA <gauravgupta1242@gmail.com>
2019-10-23 23:17:33 +0530

    Update views.py

--- quickstart/views.py
+++ quickstart/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from quickstart.serializers import UserSerializer, GroupSerializer, AruleSerializer ,  CustomeremotionSerializer
 from django.db import models
-from .models import Arule , Customeremotion
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
