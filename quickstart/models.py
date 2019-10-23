2e7f27f2b1813b26673d206acf7ec57b749cc824
GAURAV GUPTA <gauravgupta1242@gmail.com>
2019-10-23 23:08:43 +0530

    Update models.py

--- quickstart/models.py
+++ quickstart/models.py
@@ -8,29 +8,7 @@
 from django.db import models
 
 
-class Arule(models.Model):
-    index = models.BigIntegerField(blank=True, null=True) #, primary_key=True)
-    rules = models.CharField(db_column='Rules', blank=True, null=True,max_length = 100)  # Field name made lowercase.
-    support = models.TextField(db_column='Support', blank=True, null=True)  # Field name made lowercase.
-    confidence = models.TextField(db_column='Confidence', blank=True, null=True)  # Field name made lowercase.
-    lift = models.TextField(db_column='Lift', blank=True, null=True)  # Field name made lowercase.
-    target = models.CharField(db_column='Target', blank=True, null=True,max_length = 100)  # Field name made lowercase.
 
-    class Meta:
-        #managed = False
-        db_table = 'Arule'
-
-
-
-class Customeremotion(models.Model):
-    index = models.BigIntegerField(blank=True, null=True)
-    emotion = models.TextField(db_column='Emotion', blank=True, null=True)  # Field name made lowercase.
-    count = models.TextField(db_column='Count', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
-    per = models.TextField(db_column='PER', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        #managed = False
-        db_table = 'CustomerEmotion'
 
 
 
