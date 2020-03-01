91d5efcdaf850c0fe6b61fdc473be9cde912566b
Gaurav Gupta <gauravgupta1242@gmail.com>
2020-03-01 22:38:36 +0530

    Models.py Changes

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group 
 from rest_framework import serializers
-from .models 
+from quickstart import models
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
